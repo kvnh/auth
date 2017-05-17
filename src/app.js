e27bf19e8e058ad5aa8df67a39681fb6043c229f
Kevin Hackett <kevinhackett72@gmail.com>
2017-05-17 07:21:00 +0100

    Setting up firebase

--- src/app.js
+++ src/app.js
@@ -1,8 +1,20 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
+import firebase from 'firebase';
 import { Header } from './components/common';
 
 class App extends Component {
+  componentWillMount() {
+    firebase.initializeApp({
+      apiKey: 'AIzaSyCfIzPyzD5cbL-RtNFHojulu9soXHX2u_8',
+      authDomain: 'auth-18afc.firebaseapp.com',
+      databaseURL: 'https://auth-18afc.firebaseio.com',
+      projectId: 'auth-18afc',
+      storageBucket: 'auth-18afc.appspot.com',
+      messagingSenderId: '736689248806'
+    });
+  }
+
   render() {
     return (
       <View>
